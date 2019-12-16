5c820ede855e7f6b3d05a5c8870c868c458db003
Wilfried Bounsi <wilcoln99@gmail.com>
2019-12-16 11:33:57 +0100

    rien

--- src/config/Config.java
+++ src/config/Config.java
@@ -5,5 +5,5 @@ public class Config {
     public static final int PRIX_DEPLACEMENT = 1;
     public static int CHECK_TOUR_INTERVAL = 200;
     public static int REFRESH_AFFICHAGE_INTERVAL = 120; // interval entre une évolution du jeu
-    public static int STEP_INTERVAL = 800;
+    public static int STEP_INTERVAL = 400;
 }
